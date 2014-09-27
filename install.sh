7a5a6719f8d56311cfd925abd043af95f15a8819
Moritz Roth <moritz.roth11@gmail.com>
2014-09-27 12:21:07 +0100

    Add status output to installer

--- install.sh
+++ install.sh
@@ -3,17 +3,22 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Set up config folder
-mkdir -p ~/src
-cd ~/src
+echo "Setting up config folder..."
+mkdir -p ~/src && cd ~/src
 
 # Clone zsh and emacs config repositories
+echo "Cloning emacs-prelude and oh-my-zsh..."
 git clone git@github.com:mroth23/oh-my-zsh.git
 git clone git@github.com:mroth23/prelude.git
 
 # Set up symlinks in home directory
+echo "Setting up symlinks in ~..."
 cd ~
 ln -s ./src/oh-my-zsh .oh-my-zsh
 ln -s ./src/prelude .emacs.d
 
 # Copy dotfiles
+echo "Copying dotfiles..."
 cp $DIR/zshrc ~/.zshrc
+
+echo "Done!"
