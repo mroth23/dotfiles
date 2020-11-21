f2f83f6f9c515e7bd7c0cb764300f498a00d0dd8
Moritz Roth <moritz@mroth.io>
2020-11-21 02:37:06 +0100

    Switch to antigen for zsh config

--- install.sh
+++ install.sh
@@ -7,20 +7,15 @@ echo "Setting up config folder..."
 mkdir -p ~/src && cd ~/src
 
 # Clone zsh and emacs config repositories
-echo "Cloning emacs-prelude and oh-my-zsh..."
-git clone git@github.com:mroth23/oh-my-zsh.git
+echo "Cloning emacs-prelude and antigen..."
 git clone git@github.com:mroth23/prelude.git
-
-# Clone custom zsh plugins
-echo "Cloning custom zsh plugins..."
-git clone git://github.com/zsh-users/zsh-autosuggestions ./oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-completions ./oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/antigen.git
 
 # Set up symlinks in home directory
 echo "Setting up symlinks in ~..."
 cd ~
-ln -s ./src/oh-my-zsh .oh-my-zsh
 ln -s ./src/prelude .emacs.d
+ln -s $DIR/antigenrc .antigenrc
 ln -s $DIR/zshrc .zshrc
 ln -s $DIR/zshenv .zshenv
 ln -s $DIR/gitconfig .gitconfig
--- zshenv
+++ zshenv
@@ -1,2 +1,5 @@
 export PYTHONPATH=/usr/local/opt/python/libexec/bin:$PYTHONPATH
 export PATH=/usr/local/sbin:/usr/local/opt/bison/bin:$PATH
+export MANPATH="/opt/local/share/man:$MANPATH"
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export PYENV_ROOT=/usr/local/opt/pyenv
