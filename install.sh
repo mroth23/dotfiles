165a1cb3371ad60572f07e1f7a74576a57b99d3b
Moritz Roth <moritz@mroth.io>
2025-04-26 16:29:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ git clone git@github.com:mroth23/prelude.git
 
 # Clone custom zsh plugins
 echo "Cloning custom zsh plugins..."
-git clone git://github.com/zsh-users/zsh-autosuggestions ./oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-completions ./oh-my-zsh/custom/plugins/zsh-completions
+git clone git@github.com:zsh-users/zsh-autosuggestions.git ./oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-completions.git ./oh-my-zsh/custom/plugins/zsh-completions
 
 # Set up symlinks in home directory
 echo "Setting up symlinks in ~..."
